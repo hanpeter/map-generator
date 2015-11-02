a5b8067df5817f745c755af90142e8c2b66582e2
Peter Han <han.peter.621@gmail.com>
2015-11-01 23:34:17 -0800

    Body parser to handle post body

--- web.js
+++ web.js
@@ -2,10 +2,14 @@
     'use strict';
 
     var express = require('express');
+    var bodyParser = require('body-parser');
     var _ = require('lodash');
 
     var app = express();
     var PORT = process.env.PORT || 9001;
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
 
     app.listen(PORT, function () {
         console.log('Server started on port ' + PORT);
