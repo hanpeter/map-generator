926ea79a8692f67229155f43323e0352f4dba290
Peter Han <han.peter.621@gmail.com>
2015-11-01 22:43:52 -0800

    Initial commit

---
+++ web.js
@@ -0,0 +1,17 @@
+(function () {
+    'use strict';
+
+    var express = require('express');
+    var _ = require('lodash');
+
+    var app = express();
+    var PORT = process.env.PORT || 9001;
+
+    app.listen(PORT, function () {
+        console.log('Server started on port ' + PORT);
+    });
+
+    app.get('/', function (req, res) {
+        res.status(200).send();
+    });
+})();
