86e03d6cbaf5d1cd56cc522e8dd90b2136b1555b
Peter Han <phan@krux.com>
2016-03-26 23:38:05 -0700

    Route provider & ng-view

--- client/App.js
+++ client/App.js
@@ -1,27 +1,8 @@
 'use strict';
 
-var App = angular.module('App', []);
+var App = angular.module('App', ['ngRoute']);
 App.controller('AppController', require('./controllers/AppController.js'));
 App.controller('ListController', require('./controllers/ListController.js'));
+App.controller('MapController', require('./controllers/MapController.js'));
 App.factory('PlaceFactory', require('./factories/PlaceFactory.js'));
-
-// window.App = angular.module('App', ['ngRoute'])
-//     .config(['$routeProvider', function ($routeProvider) {
-//         $routeProvider
-//             .when('/:mapID/list', {
-//                 templateUrl: 'list.html',
-//                 controller: 'ListController'
-//             })
-//             .when('/:mapID/map', {
-//                 templateUrl: 'map.html',
-//                 controller: 'MapController'
-//             })
-//             .when('/:mapID', {
-//                 templateUrl: 'list.html',
-//                 controller: 'ListController'
-//             })
-//             .otherwise({
-//                 templateUrl: 'new.html',
-//                 controller: 'NewController'
-//             });
-//     }]);
+App.config(require('./configs/RouteProvider.js'));
---
+++ client/configs/RouteProvider.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var provider = function ($routeProvider) {
+    $routeProvider
+        .when('/list', {
+            templateUrl: 'templates/list.html',
+            controller: 'ListController'
+        })
+        .when('/map', {
+            templateUrl: 'templates/map.html',
+            controller: 'MapController'
+        })
+        .otherwise({
+            redirectTo: '/list'
+        });
+};
+
+provider.$inject = ['$routeProvider'];
+
+module.exports = provider;
