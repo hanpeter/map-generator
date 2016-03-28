3c6739c27245979401d54bc0a7e267e34829a7e4
Peter Han <phan@krux.com>
2016-03-27 22:13:38 -0700

    Editing places

--- client/controllers/ListController.js
+++ client/controllers/ListController.js
@@ -2,7 +2,20 @@
 
 var controller = function ($scope, PlaceFactory) {
     _.extend($scope, {
-        places: []
+        places: [],
+        editPlace: null,
+        showEditModal: function (place) {
+            $scope.editPlace = _.clone(place);
+            $('#edit-modal').modal('show');
+        },
+        saveEdit: function () {
+            PlaceFactory.updatePlace($scope.editPlace)
+                .then(function () {
+                    _.extend(place, $scope.editPlace);
+                    $('#edit-modal').modal('hide');
+                });
+            var place = _.find($scope.places, { id: $scope.editPlace.id });
+        }
     });
 
     PlaceFactory.getPlaces()
