e2b1800c4962b81827321810216e464144f7a766
Peter Han <phan@krux.com>
2016-04-16 18:33:26 -0700

    Basic Google map

--- client/controllers/MapController.js
+++ client/controllers/MapController.js
@@ -3,7 +3,10 @@
 
     var controller = function ($scope) {
         _.extend($scope, {
-
+            map: new google.maps.Map(document.getElementById('map'), {
+                center: {lat: 37.7749, lng: -122.4194},
+                zoom: 13
+            })
         });
     };
 
