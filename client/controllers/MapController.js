e3aa68119836ad8020e6cd5f64ed2d2fa50688b9
Peter Han <phan@krux.com>
2016-04-16 19:40:36 -0700

    Working POC for map

--- client/controllers/MapController.js
+++ client/controllers/MapController.js
@@ -1,16 +1,41 @@
 (function () {
     'use strict';
 
-    var controller = function ($scope) {
+    var controller = function ($scope, PlaceFactory) {
+        var geocoder = new google.maps.Geocoder();
+
         _.extend($scope, {
             map: new google.maps.Map(document.getElementById('map'), {
-                center: {lat: 37.7749, lng: -122.4194},
+                center: {lat: 34.0522, lng: -118.2437},
                 zoom: 13
-            })
+            }),
+            places: [],
         });
+
+        PlaceFactory.getPlaces()
+            .then(function (places) {
+                _.map(places, function (place) {
+                    geocoder.geocode({
+                        address: place.address
+                    }, function (res, status) {
+                        if (status === google.maps.GeocoderStatus.OK) {
+                            var infoWindow = new google.maps.InfoWindow({
+                                content: place.name
+                            });
+                            var marker = new google.maps.Marker({
+                                map: $scope.map,
+                                position: res[0].geometry.location
+                            });
+                            google.maps.event.addListener(marker, 'click', function() {
+                                infoWindow.open($scope.map, marker);
+                            });
+                        }
+                    })
+                });
+            });
     };
 
-    controller.$inject = ['$scope'];
+    controller.$inject = ['$scope', 'PlaceFactory'];
 
     module.exports = controller;
 })();
